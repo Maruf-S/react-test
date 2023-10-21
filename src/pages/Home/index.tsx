e4e41dafb73136a17aa9e200b22fb2ba04db840d
Maruf Sherefa <56218443+Maruf-S@users.noreply.github.com>
2023-10-22 01:11:02 +0300

    implement the marquee

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "../../components/Header"
 import Intro from "../../components/Intro"
+import Reference from "../../components/Reference"
 import styles from "./home.module.css"
 function Index() {
     return (
         <div className={styles["container"]}>
             <Header />
             <Intro />
+            <Reference />
         </div>
     )
 }
